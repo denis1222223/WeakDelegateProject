866abb13cf9bcd00bb31a3a187476e7038bb7e12
denis1222223 <denis1222223@mail.ru>
2016-09-22 11:42:20 +0300

    source and listener added

--- WeakDelegateProject/Program.cs
+++ WeakDelegateProject/Program.cs
@@ -8,8 +8,35 @@ namespace WeakDelegateProject
 {
     class Program
     {
+        delegate void MyDelegate(ref int a);
+        static event MyDelegate myEvent;
+
+        static void Incr(ref int a)
+        {
+            a++;
+        }
+        static void Incr2(ref int a)
+        {
+            a++;
+        }
+        static void Incr3(ref int a)
+        {
+            a++;
+        }
+
         static void Main(string[] args)
         {
+            MyDelegate del = new MyDelegate(Incr);
+
+            Console.WriteLine(myEvent.Target.ToString());
+
+            int f = 4;
+            myEvent(ref f);
+
+            Console.WriteLine(f);
+
+            Console.ReadLine();
         }
+
     }
 }
